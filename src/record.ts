736bb43543564faf9b0567b079021c44e106fbf5
Basuke Suzuki <basuke@mac.com>
2023-03-09 07:15:08 -0800

    Change FCGIRecord from class to interface.

--- src/record.ts
+++ src/record.ts
@@ -16,58 +16,68 @@ export enum Type {
 
 export const defaultAlignment = 8;
 
-export class FCGIRecord {
+export interface FCGIRecord {
     type: Type;
-    requestId: number = 0;
-    body: Buffer | null = null;
+    requestId: number;
+    body: Buffer | null;
+}
 
-    constructor(type: Type) {
-        this.type = type;
+export function makeRecord(type: Type): FCGIRecord {
+    return {
+        type,
+        requestId: 0,
+        body: null,
+    };
+}
+
+export function setBody(
+    record: FCGIRecord,
+    body: string | Buffer | null
+): void {
+    if (typeof body === 'string') {
+        record.body = Buffer.from(body);
+    } else {
+        record.body = body;
+    }
+}
+
+export function encodedSize(record: FCGIRecord): number {
+    let size = 8;
+    if (record.body) {
+        size += record.body.byteLength;
+    }
+    return size;
+}
+
+export function encode(
+    record: FCGIRecord,
+    alignment: number = defaultAlignment
+): Buffer {
+    if (alignment > 256) {
+        throw new RangeError('alignment > 256');
     }
 
-    setBody(body: string | Buffer | null) {
-        if (typeof body === 'string') {
-            this.body = Buffer.from(body);
-        } else {
-            this.body = body;
-        }
+    const size = encodedSize(record);
+    const padding = alignedSize(size, alignment) - size;
+    const buffer = Buffer.alloc(size + padding);
+    const length = record.body ? record.body.byteLength : 0;
+
+    if (length >= 0x10000) {
+        throw new RangeError('body must be < 0x10000');
     }
 
-    encodedSize(): number {
-        let size = 8;
-        if (this.body) {
-            size += this.body.byteLength;
-        }
-        return size;
+    buffer[0] = 1; // version
+    buffer[1] = record.type; // type
+    buffer[2] = hiByte(record.requestId); // requestId (Hi)
+    buffer[3] = loByte(record.requestId); // requestId (Lo)
+    buffer[4] = hiByte(length); // contentLength (Hi)
+    buffer[5] = loByte(length); // contentLength (Lo)
+    buffer[6] = padding; // paddingLength
+    buffer[7] = 0; // reserved
+
+    if (record.body) {
+        record.body.copy(buffer, 8);
     }
 
-    encode(alignment: number = defaultAlignment): Buffer {
-        if (alignment > 256) {
-            throw new RangeError('alignment > 256');
-        }
-
-        const size = this.encodedSize();
-        const padding = alignedSize(size, alignment) - size;
-        const buffer = Buffer.alloc(size + padding);
-        const length = this.body ? this.body.byteLength : 0;
-
-        if (length >= 0x10000) {
-            throw new RangeError('body must be < 0x10000');
-        }
-
-        buffer[0] = 1; // version
-        buffer[1] = this.type; // type
-        buffer[2] = hiByte(this.requestId); // requestId (Hi)
-        buffer[3] = loByte(this.requestId); // requestId (Lo)
-        buffer[4] = hiByte(length); // contentLength (Hi)
-        buffer[5] = loByte(length); // contentLength (Lo)
-        buffer[6] = padding; // paddingLength
-        buffer[7] = 0; // reserved
-
-        if (this.body) {
-            this.body.copy(buffer, 8);
-        }
-
-        return buffer;
-    }
+    return buffer;
 }
--- tests/encoder.test.ts
+++ tests/encoder.test.ts
@@ -1,5 +1,5 @@
 import { createEncoder } from '../src/encoder';
-import { FCGIRecord, Type } from '../src/record';
+import { Type, makeRecord, setBody, encode } from '../src/record';
 
 function b(...bytes: number[]): Buffer {
     return Buffer.from(bytes);
@@ -7,24 +7,24 @@ function b(...bytes: number[]): Buffer {
 
 describe('record.encode', () => {
     test('encode simple request', () => {
-        const record = new FCGIRecord(Type.FCGI_UNKNOWN_TYPE);
-        expect(record.encode()).toEqual(b(1, 11, 0, 0, 0, 0, 0, 0));
+        const record = makeRecord(Type.FCGI_UNKNOWN_TYPE);
+        expect(encode(record)).toEqual(b(1, 11, 0, 0, 0, 0, 0, 0));
     });
 
     test('encode request with body', () => {
-        const record = new FCGIRecord(Type.FCGI_UNKNOWN_TYPE);
+        const record = makeRecord(Type.FCGI_UNKNOWN_TYPE);
         record.requestId = 258;
-        record.setBody(b(0, 1, 2));
-        expect(record.encode()).toEqual(
+        setBody(record, b(0, 1, 2));
+        expect(encode(record)).toEqual(
             b(1, 11, 1, 2, 0, 3, 5, 0, 0, 1, 2, 0, 0, 0, 0, 0)
         );
     });
 
     test('encode request with string body', () => {
-        const record = new FCGIRecord(Type.FCGI_UNKNOWN_TYPE);
+        const record = makeRecord(Type.FCGI_UNKNOWN_TYPE);
         record.requestId = 259;
-        record.setBody('Hello');
-        expect(record.encode()).toEqual(
+        setBody(record, 'Hello');
+        expect(encode(record)).toEqual(
             b(1, 11, 1, 3, 0, 5, 3, 0, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0, 0, 0)
         );
     });
@@ -38,7 +38,7 @@ describe('record.encode', () => {
 //         buffers.push(blob);
 //     });
 
-//     const record = new FCGIRecord(Type.FCGI_UNKNOWN_TYPE);
+//     const record = makeRecord(Type.FCGI_UNKNOWN_TYPE);
 //     encoder.feed(record);
 //     expect(buffers).toEqual([b(0, 11, 0, 0, 0, 0, 0, 0)]);
 // });
