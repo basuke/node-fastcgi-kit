906c6ae7bb08956e284990049b4e50011909da3d
Basuke Suzuki <basuke@mac.com>
2023-03-19 10:58:43 -0700

    Added key-value decoder test.

--- src/keyvalues.ts
+++ src/keyvalues.ts
@@ -1,6 +1,6 @@
 export type Pairs = { [name: string]: string };
 
-function encodeLengthOf(buffer: Buffer): Buffer {
+function encodeLength(buffer: Buffer): Buffer {
     const len = buffer.byteLength;
     return Buffer.from(
         len >= 128
@@ -14,6 +14,44 @@ function encodeLengthOf(buffer: Buffer): Buffer {
     );
 }
 
+function decodePair(
+    buffer: Buffer,
+    offset: number
+): [string, string, number] | undefined {
+    function decodeLength(): number | undefined {
+        if (buffer.byteLength <= offset) return undefined;
+
+        const length = buffer[offset];
+        if (length < 0x80) {
+            offset += 1;
+            return length;
+        }
+        if (buffer.byteLength <= offset + 3) return undefined;
+        const longLength =
+            ((length & 0x7f) << 24) +
+            (buffer[offset + 1] << 16) +
+            (buffer[offset + 2] << 8) +
+            buffer[offset + 3];
+        offset += 4;
+        return longLength;
+    }
+
+    const nameLength = decodeLength();
+    if (nameLength === undefined) return undefined;
+    const valueLength = decodeLength();
+    if (valueLength === undefined) return undefined;
+    if (buffer.byteLength < offset + nameLength + valueLength) return undefined;
+
+    const nameStart = offset;
+    const valueStart = nameStart + nameLength;
+    const valueEnd = valueStart + valueLength;
+    return [
+        buffer.subarray(nameStart, valueStart).toString(),
+        buffer.subarray(valueStart, valueEnd).toString(),
+        valueEnd,
+    ];
+}
+
 export function encode(pairs: Pairs): Buffer {
     const buffers: Buffer[] = [];
 
@@ -23,10 +61,25 @@ export function encode(pairs: Pairs): Buffer {
         const nameBytes = Buffer.from(name);
         const valueBytes = Buffer.from(value);
 
-        buffers.push(encodeLengthOf(nameBytes));
-        buffers.push(encodeLengthOf(valueBytes));
+        buffers.push(encodeLength(nameBytes));
+        buffers.push(encodeLength(valueBytes));
         buffers.push(nameBytes);
         buffers.push(valueBytes);
     }
     return Buffer.concat(buffers);
+}
+
+export function decode(buffer: Buffer, pairs: Pairs): Buffer | null {
+    let offset = 0;
+    while (offset < buffer.byteLength) {
+        const result = decodePair(buffer, offset);
+        if (result === undefined) {
+            return offset > 0 ? buffer.subarray(offset) : buffer;
+        }
+
+        const [name, value, newOffset] = result;
+        pairs[name] = value;
+        offset = newOffset;
+    }
+    return null;
 }
--- tests/keyvalues.test.ts
+++ tests/keyvalues.test.ts
@@ -1,4 +1,4 @@
-import { encode, Pairs } from '../src/keyvalues';
+import { decode, encode, Pairs } from '../src/keyvalues';
 import { bytestr as B } from '../src/utils';
 
 describe('Key-Value paire encoding', () => {
@@ -32,5 +32,42 @@ describe('Key-Value paire encoding', () => {
         const values: Pairs = {};
         values[name] = 'hello';
         expect(encode(values)).toEqual(B`${[128, 0, 1, 4]}05${name}${'hello'}`);
+    });
+});
+
+describe('Decoding key-value pairs', () => {
+    test('simple pair', () => {
+        const pairs = {};
+        const remainings = decode(B`${[5, 5]} ${'hello'} ${'world'}`, pairs);
+        expect(pairs).toEqual({ hello: 'world' });
+        expect(remainings).toBeNull();
+    });
+
+    test('multiple pairs', () => {
+        const pairs = {};
+        decode(B`${[5, 5]} ${'helloworld'} ${[3, 3]} ${'foobar'}`, pairs);
+        expect(pairs).toEqual({ hello: 'world', foo: 'bar' });
+    });
+
+    test('extra data in buffer', () => {
+        const pairs = {};
+        const remainings = decode(
+            B`${[5, 5]} ${'helloworld'} ${[3, 3]}`,
+            pairs
+        );
+        expect(pairs).toEqual({ hello: 'world' });
+        expect(remainings).toEqual(Buffer.from([3, 3]));
+    });
+
+    test('not enough data', () => {
+        const pairs = {};
+        decode(B``, pairs);
+        expect(pairs).toEqual({});
+
+        decode(B`${[5, 5]}`, pairs);
+        expect(pairs).toEqual({});
+
+        decode(B`${[5, 5]} ${'Hello'}`, pairs);
+        expect(pairs).toEqual({});
     });
 });
