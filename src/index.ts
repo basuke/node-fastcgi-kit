067e89b5170884ca634eb18fa18de886b86a9590
Basuke Suzuki <basuke@mac.com>
2023-04-14 12:02:35 -0700

    expose requestToParams

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { createClient } from './client';
 export type { Client, Request, Response } from './client';
+export { requestToParams, urlToParams } from './options';
 export type { ClientOptions } from './options';
 export { encode as encodeParams, StreamDecoder } from './params';
 export type { Params } from './params';
